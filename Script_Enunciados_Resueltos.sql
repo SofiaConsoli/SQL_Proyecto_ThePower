5e97e0d2b81483640e443de29a8170a637303cd4
SofiaConsoli <sofiaconsoli13@gmail.com>
2026-01-16 17:43:41 +0100

    4th update script
    hasta el enunciado 25

--- Script_Enunciados_Resueltos.sql
+++ Script_Enunciados_Resueltos.sql
@@ -76,8 +76,9 @@
 
 --17. Encuentra el nombre y apellido de los actores que aparecen en la película con título ‘Egg Igby’.
 --información que necesito: (film_id . title) de film (film_id . actor_id) de film_actor (actor_id . first_name . last_name) de actor	
-	
-	select a."first_name" , a."last_name"
+--Dejo detalladas dos versiones debido a que llegué al mismo resultado de dos formas diferentes, luego de ver en clase una forma más simplificada de lo que había desarrollado
+	--versión 1:
+select a."first_name" , a."last_name"
 	from "actor" as "a"
 	inner join (
 	(select f."film_id" , fa."actor_id" ,f."title"
@@ -86,12 +87,21 @@
 		"film_actor" as "fa" on fa."film_id" = f."film_id")) as "ei" on a."actor_id" = ei."actor_id"
 	where ei."title" = 'EGG IGBY';
 	
+	--versión 2 simplificada
+	select a."first_name", a."last_name"
+	from "actor" as "a"
+	inner join "film_actor" as "fa" on fa."actor_id" = a."actor_id"
+	inner join "film" as "f" on f."film_id" = fa."film_id"
+	where f."title" = 'EGG IGBY';
+	
 --18. Selecciona todos los nombres de las películas únicos.
 	select distinct("title")
 	from "film";
 
 --19. Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla “film”.
 -- información que necesito: (film_id . title) de film (film_id . category_id) de film_category (category_id . name) category
+--Dejo detalladas dos versiones debido a que llegué al mismo resultado de dos formas diferentes, luego de ver en clase una forma más simplificada de lo que había desarrollado
+	--versión 1
 	select cat."title"
 	from "category" as "c"
 	inner join (
@@ -101,11 +111,61 @@
 		"film_category" as "fc" on fc."film_id" = f."film_id")) as "cat" on c.category_id = cat.category_id
 	where c."name" = 'Comedy' and cat."length" > 180;
 
---20. 
+	--versión 2 simplificada
+	select f."title"
+	from "film" as "f"
+	inner join film_category "fc" on fc."film_id" = f."film_id"
+	inner join "category" as "c" on c."category_id" = fc."category_id"
+	where c."name" = 'Comedy'
+  	and f."length" > 180;
 
-	select *
-	from "film";
-	select *
-	from "film_category";
-	select*
-	from "category";
+
+--20. Encuentra las categorías de películas que tienen un promedio de duración superior a 110 minutos y muestra el nombre de la categoría junto con el promedio de duración.
+--Dejo detalladas dos versiones debido a que llegué al mismo resultado de dos formas diferentes, luego de ver en clase una forma más simplificada de lo que había desarrollado
+	--versión 1
+	select avg(cat."length") as "promedio_duración"  , c."name" 
+	from "category" as "c"
+	inner join (
+	(select f."film_id" , fc."category_id", f."title" , f."length" 
+	from "film" as "f"
+	inner join 
+		"film_category" as "fc" on fc."film_id" = f."film_id")) as "cat" on c.category_id = cat.category_id
+	group by c."name" 
+	having  avg(cat."length") > 110;
+
+	--versión 2 simplificada
+	select c."name" AS categoria, avg(f."length") as "promedio_duracion"
+	from "category" as c
+	left join film_category fc on fc.category_id = c.category_id
+	left join film f on f.film_id = fc.film_id
+	group by c."name"
+	having avg(f."length") > 110;
+	
+--21. ¿Cuál es la media de duración del alquiler de las películas?
+	select avg("return_date" - "rental_date") as "media_duracion_alquiler"
+	from "rental";
+
+--22. Crea una columna con el nombre y apellidos de todos los actores y actrices.
+	select concat("first_name" , ', ' , "last_name") as "nombre_completo"
+	from "actor";
+
+--23. Números de alquiler por día, ordenados por cantidad de alquiler de forma descendente.
+	select count("rental_id") , "rental_date"
+	from "rental"
+	group by "rental_date"
+	order by count("rental_id") desc;
+
+--24. Encuentra las películas con una duración superior al promedio.
+	select "title"
+	from "film"
+	where "length" > (select avg("length")
+    from "film");
+
+--25. Averigua el número de alquileres registrados por mes.
+	select date_trunc('month', "rental_date") as "mes", count("rental_id") as "cantidad_alquileres"
+	from "rental"
+	group by date_trunc('month', "rental_date");
+
+--26. 
+
+
